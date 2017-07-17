a51f8b07f20d06015ebac224172367336fce746c
albert <albert.chenwesoft@gmail.com>
2017-07-17 18:27:35 +0800

    新增笔记本端的

--- ClientDemo.py
+++ ClientDemo.py
@@ -6,15 +6,6 @@
 @time: 2017/07/17/17:54
 """
 
-#!usr/bin/env python
-
-#-*- coding:utf-8 _*-
-"""
-@author:albert.chen
-@file: drag.py
-@time: 2017/07/17/16:09
-"""
-
 import pyautogui
 import time
 import subprocess
@@ -31,30 +22,44 @@ import subprocess
 
 # print pyautogui.size()
 
-p=subprocess.Popen("C:\\Program Files (x86)\\Inpor\\4UClient\\FMDesktop\\FastMeeting.exe", shell=True)
+p=subprocess.Popen("D:\\softinstall\\4UClient\\FMDesktop\\FastMeeting.exe", shell=True)
 
+time.sleep(1)
+if pyautogui.size()[0]==1366:
+    pyautogui.click(612, 400)
+else:
+    pyautogui.click(923, 546)
+
+pyautogui.press("backspace")
 time.sleep(2)
-pyautogui.click(923, 546)
-
 pyautogui.typewrite('fm01\n',interval=0.1)
 
-time.sleep(2)
+time.sleep(1)
 
+pyautogui.hotkey('ctrl','a')
+pyautogui.press("backspace")
+time.sleep(2)
 pyautogui.typewrite('000000\n')
 
-time.sleep(2)
+time.sleep(3)
 
-pyautogui.moveTo(1095, 662)
+if pyautogui.size()[0]==1366:
+    pyautogui.moveTo(811, 510)
+else:
+    pyautogui.moveTo(1095, 662)
 
-time.sleep(2)
+time.sleep(1)
 
 pyautogui.click()
 
-time.sleep(2)
+time.sleep(3)
 
-pyautogui.moveTo(188, 193)
+if pyautogui.size()[0]==1366:
+    pyautogui.moveTo(188, 193)
+else:
+    pyautogui.moveTo(188, 193)
 
-time.sleep(1)
+time.sleep(3)
 
 pyautogui.click()
 
